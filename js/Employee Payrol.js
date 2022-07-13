9140298b123d37bfed12d3719088261be869d11d
unknown <harini.lekkala1997@gmail.com>
2022-07-13 14:45:04 +0530

    [Harini]: UC-8 Adding Event Listner

--- js/Employee Payrol.js
+++ js/Employee Payrol.js
@@ -4,3 +4,20 @@ const message = document.querySelector('.salary-output');
 salary.addEventListener('input', function(){
     message.textContent = salary.value;
 });
+window.addEventListener('DOMContentLoaded',(event) =>{
+    const  NAME = document.querySelector('#name');
+    const ERROR_OP = document.querySelector('.error-output');
+    NAME.addEventListener('input',function(){
+        if(NAME.value.length == 0){
+            ERROR_OP.textContent = "";
+            return;
+        }
+        try{
+            (new PayrollClass()).empName = NAME.value;
+            ERROR_OP.textContent = "";
+        }
+        catch(e){
+            ERROR_OP.textContent = e;
+        }
+    });
+});
