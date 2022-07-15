d52f0d2048d3a912cb01eda080a4d1058a15f504
unknown <harini.lekkala1997@gmail.com>
2022-07-15 17:20:20 +0530

    [Harini]: UC-17 Displaying Details From LocalStorage

---
+++ js/HomePage.js
@@ -0,0 +1,121 @@
+let employeePayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
+    createInnerHtml();
+});
+
+
+const getEmployeePayrollDataFromStorage = () =>{
+    return localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+                        
+                        
+}
+
+
+// Save data to sessionStorage
+//localStorage.setItem('key', 'value');
+
+// Get saved data from sessionStorage
+//var data = localStorage.getItem('key');
+
+// Remove saved data from sessionStorage
+//localStorage.removeItem('key');
+
+// Remove all saved data from sessionStorage
+//localStorage.clear();
+
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    
+    let innerHtml = `${headerHtml}`;
+    //let empPayrollList = createEmployeePayrollJSON();-------------uc15-------
+    for (const employeePayroll of employeePayrollList) {
+        innerHtml = `${innerHtml}  
+        <tr>
+            <td><img class="profile" alt=""
+                src="${employeePayroll._profilePic  }">
+            </td>
+            <td>${employeePayroll._name}</td>
+            <td>${employeePayroll._gender}</td>
+            <td>${getDeptHtml(employeePayroll._department)}</td>
+            <td>${employeePayroll._salary}</td>
+            <td>${employeePayroll._startDate}</td>
+            <td>
+                <img id="${employeePayroll._id}" onclick="remove(this)" alt="delete" 
+                    src="../assests/trashbin.jpeg">
+                <img id="${employeePayroll._id}" onclick="update(this)" alt="edit"
+                    src="../assests/add.jpeg">
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+/*-----------uc15----------------*/
+/*const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+        {
+            _name: 'Prasanna',
+            _gender: 'Female',
+            _department: [
+                'Analyst',
+                'Engineer'
+            ],
+            _salary: '650000',
+            _startDate: '8 Nov 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/Profile1.jpg'
+        },
+        {
+            _name: 'Arjun',
+            _gender: 'Male',
+            _department: [
+                'Army'
+            ],
+            _salary: '800000',
+            _startDate: '15 Jan 2015',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/Profile2.jpg'
+        },
+        {
+            _name: 'Dimple',
+            _gender: 'Female',
+            _department: [
+                'Teacher',
+                'Engineer'
+            ],
+            _salary: '900000',
+            _startDate: '25 Nov 2021',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/Profile3.jpg'
+        },
+        {
+            _name: 'Mallikharjun',
+            _gender: 'Male',
+            _department: [
+                'Sales',
+                'Engineer'
+            ],
+            _salary: '700000',
+            _startDate: '5 Feb 2017',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/Profile4.jpg'
+        }
+    ];
+    return employeePayrollListLocal;
+}*/
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
