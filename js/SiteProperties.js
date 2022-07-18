fca3351780b9bf54ee9c34acddc839c8b09e1c3c
unknown <harini.lekkala1997@gmail.com>
2022-07-18 15:47:45 +0530

    [Harini]: Doing CRUD Operations And Saving In Local Storagr And Displaying In Form

---
+++ js/SiteProperties.js
@@ -0,0 +1,5 @@
+const SiteProperties = {
+    LoginPage: "../pages/Login.html",
+    HomePage: "../pages/HomePage.html",
+    AddEmployee: "../pages/AddEmployee.html"
+};
--- js/Site_Properties.js
+++
@@ -1,4 +0,0 @@
-let Site_Properties = {
-    home_page: "../pages/HomePage.html",
-    add_emp_payroll_page: "../pages/Payroll_Form.html"
-};
